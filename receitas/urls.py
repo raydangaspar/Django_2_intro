6ea08baf197d9e986e47a6e357f880bed09a6d1e
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-04-26 14:26:56 -0300

    Links, urls e views

--- receitas/urls.py
+++ receitas/urls.py
@@ -5,5 +5,6 @@ from . import views
 # primeiro parâmetro é a rota. Segundo parâmetro é o view.
 # Terceiro parâmetro namespace do aplicativo para a entrada url
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('receita', views.receita, name='receita')
 ]
