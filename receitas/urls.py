add767f1331e4297aa757787450d3f5340f53aa0
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-04-26 10:32:22 -0300

    Servidor e app de receita

---
+++ receitas/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+# primeiro parâmetro é a rota. Segundo parâmetro é o view.
+# Terceiro parâmetro namespace do aplicativo para a entrada url
+urlpatterns = [
+    path('', views.index, name='index')
+]
