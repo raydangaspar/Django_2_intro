a31146cfc2c186f124033fab4ad25da1392c08c8
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-07 15:11:20 -0300

    Mensagens de sucesso e erro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 from receitas.models import Receita
 
 # Create your views here.
@@ -17,15 +17,15 @@ def cadastro(request):
             print('O campo email não pode ficar em branco')
             return redirect('cadastro')
         if senha != senha2:
-            print('As senhas não são iguais')
+            messages.error(request, 'As senhas não são iguais')
             return redirect('cadastro') 
         if User.objects.filter(email=email).exists():
             print('Usuário já cadastrado')
             return redirect('cadastro')
         user = User.objects.create_user(username=nome, email=email, password=senha)
         user.save()
-        print('Usuário cadastrado com sucesso')
-        print('Usuário criado com sucesso')
+
+        messages.success(request, 'Usuário cadastrado com sucesso')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
