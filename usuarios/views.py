fdd4323d9b464b6a4fb6afe61f93dcd5fa1c676e
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-07 14:50:14 -0300

    Receitas de cada usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
 from receitas.models import Receita
@@ -53,7 +53,14 @@ def logout(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, 'usuarios/dashboard.html')
+        id = request.user.id
+        receitas = Receita.objects.order_by('-date_receita').filter(pessoa=id)
+
+        dados = {
+            'receitas' : receitas
+        }
+
+        return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
 
@@ -66,10 +73,21 @@ def cria_receita(request):
         rendimento = request.POST['rendimento']
         categoria = request.POST['categoria']
         foto_receita = request.FILES['foto_receita']
-        print(nome_receita, ingredientes, modo_preparo, 
-        tempo_preparo, rendimento, categoria, foto_receita)
 
-        # receita = Receita.objects.create(pessoa=)
+        user = get_object_or_404(User, pk=request.user.id)
+        
+        receita = Receita.objects.create(
+            pessoa=user,
+            nome_receita=nome_receita,
+            ingredientes=ingredientes,
+            modo_preparo=modo_preparo,
+            tempo_preparo=tempo_preparo,
+            rendimento=rendimento,
+            categoria=categoria,
+            foto_receita=foto_receita
+        )
+        
+        receita.save()
         return redirect('dashboard')
     else:
         return render(request, 'usuarios/cria_receita.html')
